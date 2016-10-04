d8a2d9330c38b5da1f8cd3132d241dc27b0b2ddb
ry <atpapers@gmail.com>
2016-10-04 14:28:28 -0700

    Read Json Finish

--- app/src/main/java/nakthon/soraya/driverry/MainActivity.java
+++ app/src/main/java/nakthon/soraya/driverry/MainActivity.java
@@ -1,10 +1,17 @@
 package nakthon.soraya.driverry;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
                     //No Space
-
+                    Authen authen = new Authen(MainActivity.this);
+                    authen.execute("http://swiftcodingthai.com/ry/get_data_ry.php");
                 }
 
             } //onClick
@@ -52,5 +60,44 @@ public class MainActivity extends AppCompatActivity {
 
 
     } // Main Method
+
+    private class Authen extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+
+        public Authen(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(params[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                Log.d("DriverV1", "e doInBack ==>" + e.toString());
+                return null;
+            }
+
+
+        }   //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("DriverV1", "Json ==>" + s);
+
+        } // onPost
+
+    } //Authen Class
 
 } // Main Class
