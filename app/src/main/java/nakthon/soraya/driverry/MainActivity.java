ab6e96f96bebe17f5c0626a9baff544efd75ac5c
ry <atpapers@gmail.com>
2016-10-04 13:15:19 -0700

    Check Space Finish

--- app/src/main/java/nakthon/soraya/driverry/MainActivity.java
+++ app/src/main/java/nakthon/soraya/driverry/MainActivity.java
@@ -2,12 +2,55 @@ package nakthon.soraya.driverry;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private Button signInButton, signUpButton;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Windget
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText);
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //signIn Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //Get Value from Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //Check Space
+                if (userString.equals("") || passwordString.equals("")) {
+                    //Have Space
+
+                    MyAlert myAlert = new MyAlert(MainActivity.this,
+                            R.drawable.bird48, "Have Space",
+                            "Please Fill All Every Blank");
+                    myAlert.myDialog();
+
+                } else {
+                    //No Space
+
+                }
+
+            } //onClick
+        });
+
+
+
+    } // Main Method
+
+} // Main Class
