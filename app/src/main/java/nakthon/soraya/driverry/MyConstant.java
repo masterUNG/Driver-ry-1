c7255580a379aa0eb8d78f9044cf673f00635254
ry <atpapers@gmail.com>
2016-11-07 15:26:44 +0700

    Read JSON finish

--- app/src/main/java/nakthon/soraya/driverry/MyConstant.java
+++ app/src/main/java/nakthon/soraya/driverry/MyConstant.java
@@ -17,6 +17,12 @@ public class MyConstant {
             "Phone",
             "Id_car"};
 
+    private String urlGetJobWhereID = "http://swiftcodingthai.com/ry/get_job_where_idPass_and_Status.php";
+
+    public String getUrlGetJobWhereID() {
+        return urlGetJobWhereID;
+    }
+
     public String[] getUserStrings() {
         return userStrings;
     }
--- app/src/main/java/nakthon/soraya/driverry/ServiceActivity.java
+++ app/src/main/java/nakthon/soraya/driverry/ServiceActivity.java
@@ -1,7 +1,13 @@
 package nakthon.soraya.driverry;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,32 +15,99 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
 public class ServiceActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    //Explicit
     private GoogleMap mMap;
+    private TextView nameTextView, phoneTextView, dateTextView, timeTextView;
+    private ImageView imageView;
+    private Button button;
+    private String[] loginStrings;
+    private MyConstant myConstant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_service_layout);
+
+        //Bind WIdget
+        nameTextView = (TextView) findViewById(R.id.textView3);
+        phoneTextView = (TextView) findViewById(R.id.textView4);
+        dateTextView = (TextView) findViewById(R.id.textView5);
+        timeTextView = (TextView) findViewById(R.id.textView6);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        button = (Button) findViewById(R.id.button2);
+
+        //Get Value From Intent
+        loginStrings = getIntent().getStringArrayExtra("Login");
+        Log.d("7novV1", "id_Passenger ==>" + loginStrings[0]);
+
+        //Get Value From JSON
+        myConstant = new MyConstant();
+        GetJob getJob = new GetJob(ServiceActivity.this);
+        getJob.execute(myConstant.getUrlGetJobWhereID());
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
+    }   //Main Method
+
+    private class GetJob extends AsyncTask<String, Void, String> {
+
+
+        //Explicit
+        private Context context;
+
+        public GetJob(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("ID_passenger", loginStrings[0])
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+
+        }   //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("7novV1", "Result ==>" + s);
+
+        }   // onPost
+
+    }   //GetJob Class
+
+
+
+       @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
@@ -42,5 +115,6 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+    }  //onMapReady
+
+}  //Main Class
