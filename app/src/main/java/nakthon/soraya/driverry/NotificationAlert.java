172b9d965f6ee5739987be47f8e2d270adc670fe
masterUNG <phrombutr@gmail.com>
2016-12-02 16:48:48 +0700

    ConfirmJob near OK

--- app/src/main/java/nakthon/soraya/driverry/NotificationAlert.java
+++ app/src/main/java/nakthon/soraya/driverry/NotificationAlert.java
@@ -1,13 +1,49 @@
 package nakthon.soraya.driverry;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificationAlert extends AppCompatActivity {
+
+    //Explicit
+    private Button okButton, noButton;
+    private String[] loginStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_alert);
-    }
-}
+
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
+        //Bind Widget
+        okButton = (Button) findViewById(R.id.button5);
+        noButton = (Button) findViewById(R.id.button6);
+
+        //no Controller
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+        //ok Controller
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificationAlert.this, ServiceActivity.class);
+                intent.putExtra("Login", loginStrings);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }   // Main Method
+
+}   // Main Class
--- app/src/main/java/nakthon/soraya/driverry/PhotoActivity.java
+++ app/src/main/java/nakthon/soraya/driverry/PhotoActivity.java
@@ -7,11 +7,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Handler;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -106,7 +105,7 @@ public class PhotoActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        // super.onBackPressed();
+         super.onBackPressed();
     }
 
     private void upLoadToServer() {
